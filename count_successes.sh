4cf25b7798d2a21832e7b541c9d7480ecbc5b26e
Gregory Petterson <pet03356@morris.umn.edu>
2022-09-05 15:14:18 -0500

    o Ple bunch of stuff out but didn't know how to work nano.

--- count_successes.sh
+++ count_successes.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-echo "There were 30 successes and 80 failures."
+tar_file=$1
+
+SCRATCH=$(mktemp -d)
+
+tar -zxf "$tar_file" --directory="$SCRATCH"
+
+num_successes=$(grep -r -l "SUCCESS" "$SCRATCH" | wc -l)
+num_failures=$(grep -r -l "FAILURE" "$SCRATCH" | wc -l)
+
+echo "There were $num_successes successes and $num_failures failures."
+
+rm -rf "$SCRATCH"
